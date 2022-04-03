c660408de85c0257a7882f238daf097359e9014d
Md-Tur <turibnmostafiz@gmail.com>
2022-04-04 05:27:16 +1000

    navbar,product section & review section added

--- src/components/HOME/HOME.js
+++ src/components/HOME/HOME.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h1>it's home.</h1>
+            <div>
+                <div>
+                    <h3>It could be your best product</h3>
+                    <h3 className='tagline'>With good value for money</h3>
+                    <p>choose it & save money.</p>
+                    <button className='live-demo'>Live Demo</button>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+
+            <div>
+                <h2>Customer Reviews</h2>
+                <button className='review-details'>See All Reviews</button>
+            </div>
         </div>
     );
 };
